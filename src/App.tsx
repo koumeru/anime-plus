dd0f0c670c6ae97b903fb20925d55668ba2d16bf
kou <kou.pryvate9511ok@gmail.com>
2025-07-14 23:50:12 +0900

    ページネーション機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useEffect, useState } from 'react'
 import Header from './components/Header/Header'
 import AnimeCard from './components/Contetnts/AnimeCard'
 import SeachAnime from './components/Contetnts/SeachAnime'
+import Pagenation from './components/Contetnts/Pagenation'
 import useSWR from 'swr'
 import axios from 'axios'
 
@@ -22,6 +23,16 @@ export const useAnimeList = () => {
 	return context
 }
 
+// ページのコンテキスト設定
+const PageContext = createContext<[number, React.Dispatch<React.SetStateAction<number>>]>([
+    1, 
+    () => {}
+])
+export const usePage = () => {
+	const context = useContext(PageContext)
+	return context
+}
+
 // APIからアニメデータを取得する関数（useSWRを使って高速化）
 async function fetcher(key: string) {
 	const response = await axios.get<any>(key)
@@ -32,35 +43,34 @@ function App() {
 	const [keyword, setKeyword] = useState('')
 	const [page, setPage] = useState(1)
 	const per_page = 20
+	const [url, seturl] = useState('https://api.annict.com/v1/works?access_token=')
 
 	// useSWRでアニメデータを取得
-	const {
-		data: animeList,
-		error,
-		isLoading,
-	} = useSWR(
-		`https://api.annict.com/v1/works?access_token=${
+	const { data: animeList, isLoading } = useSWR(
+		`${url}${
 			import.meta.env.VITE_ANNICT_API_KEY
 		}&sort_watchers_count=desc&page=${page}&per_page=${per_page}&filter_title=${keyword}`,
 		fetcher
 	)
 	console.log('アニメリスト:', animeList)
 
+	useEffect(() => {
+		setPage(1) // キーワードが変わったらページをリセット
+	}, [keyword])
+
 	// フロントエンド
 	return (
 		<>
 			<Header />
 			<KeywordContext.Provider value={[keyword, setKeyword]}>
-				<AnimeListContext.Provider value={[isLoading? [] : animeList]}>
+                <PageContext.Provider value={[page, setPage]}>
+				<AnimeListContext.Provider value={[isLoading ? [] : animeList]}>
 					<SeachAnime />
 					<AnimeCard />
+					<Pagenation />
 				</AnimeListContext.Provider>
+                </PageContext.Provider>
 			</KeywordContext.Provider>
-			{/* ページネーション */}
-			<button onClick={() => setPage(page - 1)} disabled={page <= 1}>
-				前へ
-			</button>
-			<button onClick={() => setPage(page + 1)}>次へ</button>
 		</>
 	)
 }
